b03143cc4f9555b70802be922fa6411f574102c8
Keith Rozario <keith@keithrozario.com>
2019-02-17 18:09:31 +0800

    Added explicit exceptions to be caught

--- certlib.py
+++ certlib.py
@@ -1,9 +1,7 @@
 import base64
-import math
-
 import datetime
-
 from OpenSSL import crypto
+
 
 CTL_LISTS = 'https://www.gstatic.com/ct/log_list/log_list.json'
 
@@ -38,74 +36,6 @@ PreCertEntry = Struct(
 )
 
 
-async def retrieve_all_ctls(session=None):
-    async with session.get(CTL_LISTS) as response:
-        ctl_lists = await response.json()
-
-        logs = ctl_lists['logs']
-
-        for log in logs:
-            if log['url'].endswith('/'):
-                log['url'] = log['url'][:-1]
-            owner = _get_owner(log, ctl_lists['operators'])
-            log['operated_by'] = owner
-
-        return logs
-
-
-def _get_owner(log, owners):
-    owner_id = log['operated_by'][0]
-    owner = next(x for x in owners if x['id'] == owner_id)
-    return owner['name']
-
-
-async def get_max_block_size(log, session):
-    async with session.get(DOWNLOAD.format(log['url'], 0, 10000)) as response:
-        entries = await response.json()
-        return len(entries['entries'])
-
-
-async def retrieve_log_info(log, session):
-    block_size = await get_max_block_size(log, session)
-
-    async with session.get(CTL_INFO.format(log['url'])) as response:
-        info = await response.json()
-        info['block_size'] = block_size
-        info.update(log)
-        return info
-
-
-async def populate_work(work_deque, log_info, start=0):
-    tree_size = log_info['tree_size']
-    block_size = log_info['block_size']
-
-    total_size = tree_size - 1
-
-    end = start + block_size
-
-    if end > tree_size:
-        end = tree_size
-
-    chunks = math.ceil((total_size - start) / block_size)
-
-    if chunks == 0:
-        raise Exception("No work needed!")
-
-    for _ in range(chunks):
-        # Cap the end to the last record in the DB
-        if end >= tree_size:
-            end = tree_size - 1
-
-        assert end >= start, "End {} is less than start {}!".format(end, start)
-        assert end < tree_size, "End {} is less than tree_size {}".format(end, tree_size)
-
-        work_deque.append((start, end))
-
-        start += block_size
-
-        end = start + block_size + 1
-
-
 def add_all_domains(cert_data):
     all_domains = []
 
@@ -125,16 +55,8 @@ def add_all_domains(cert_data):
 
 def dump_cert(certificate):
     subject = certificate.get_subject()
-
-    try:
-        not_before = datetime.datetime.strptime(certificate.get_notBefore().decode('ascii'), "%Y%m%d%H%M%SZ").timestamp()
-    except:
-        not_before = 0
-
-    try:
-        not_after = datetime.datetime.strptime(certificate.get_notAfter().decode('ascii'), "%Y%m%d%H%M%SZ").timestamp()
-    except:
-        not_after = 0
+    not_before = datetime.datetime.strptime(certificate.get_notBefore().decode('ascii'), "%Y%m%d%H%M%SZ").timestamp()
+    not_after = datetime.datetime.strptime(certificate.get_notAfter().decode('ascii'), "%Y%m%d%H%M%SZ").timestamp()
 
     return {
         "subject": {
@@ -159,14 +81,16 @@ def dump_extensions(certificate):
         extension_name = ""
         try:
             extension_name = certificate.get_extension(x).get_short_name()
-
-            if extension_name == b'UNDEF':
-                continue
-
-            extensions[extension_name.decode('latin-1')] = certificate.get_extension(x).__str__()
-        except:
+            if extension_name != b'UNDEF':
+                extensions[extension_name.decode('latin-1')] = certificate.get_extension(x).__str__()
+        except IndexError:
+            # IndexError raised when trying to access unknown cert
+            pass
+        except (crypto.Error, UnicodeDecodeError):
+            # crypto.error raised when found by OpenSSL, but PyOpenSSL has no parsing logic
+            # UnicodeDecodeError raised when unable to decode cert extension
             try:
                 extensions[extension_name.decode('latin-1')] = "NULL"
-            except Exception as e:
+            except UnicodeDecodeError:
                 pass
     return extensions
