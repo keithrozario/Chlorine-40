fd56c596e83cccbf521406c65f20e531faa51be1
Keith Rozario <keith@keithrozario.com>
2019-03-04 23:13:05 +0800

    Added Command Line Arguments

--- invoke_query_db.py
+++ invoke_query_db.py
@@ -1,9 +1,12 @@
+#! ./venv/bin/python
+
 import boto3
 import json
 import logging
 import uuid
 from itertools import product
 from string import ascii_lowercase
+import argparse
 
 from invocations import get_config, get_ssm
 
@@ -21,7 +24,19 @@ if __name__ == '__main__':
     console.setLevel(logging.INFO)
     logger.addHandler(console)
 
-    env = 'dev'
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--environment",
+                        help="Stage to query",
+                        default='default')
+    parser.add_argument("-i", "--initials",
+                        help="Initials to query",
+                        default='all')
+    args = parser.parse_args()
+
+    env = 'default'
+    initials = args.initials
+
     config = get_config()
     aws_region = config['aws_region'][env]
     app_name = config['app_name']
@@ -30,9 +45,10 @@ if __name__ == '__main__':
     client = boto3.client('sqs', region_name=aws_region)
     max_batch_size = 10
 
-    # keywords = map(''.join, product(ascii_lowercase + '0123456789-', repeat=2))
-
-    keywords = ['xn']
+    if initials == 'all':
+        keywords = map(''.join, product(ascii_lowercase + '0123456789-', repeat=2))
+    else:
+        keywords = [initials]
 
     message_bodies = [{"initials": keyword} for keyword in keywords]
 
@@ -41,7 +57,7 @@ if __name__ == '__main__':
 
     for k in range(0, len(message_batch), max_batch_size):
         response = client.send_message_batch(QueueUrl=que_url,
-                                             Entries=message_batch[k:k + max_batch_size])
+                                             Entries=message_batch[k: (k + max_batch_size)])
 
         if len(response.get('Successful', [])) == max_batch_size:
             logger.info("{} successful, {} total messages sent".format(len(response.get('Successful', [])),
