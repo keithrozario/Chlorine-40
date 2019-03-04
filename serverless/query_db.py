13c9527f9a3c27d9c8402df5c9440112aafe2875
Keith Rozario <keith@keithrozario.com>
2019-03-04 23:11:37 +0800

    fixed size issue reporting

--- serverless/query_db.py
+++ serverless/query_db.py
@@ -32,6 +32,7 @@ def main(event, context):
 
     domains = []
 
+    # Query the temp DB for all records with initials
     print("Querying DB..")
     response = table.query(
         KeyConditionExpression=Key('initials').eq(initials)
@@ -47,6 +48,7 @@ def main(event, context):
         for item in response.get('Items', []):
             domains.extend(json.loads(item['domains']))
 
+    # Make list unique across all domains queried
     unique_db_domains = list(set(domains))
     print("{} calls made, {} domains found,  {} are unique".format(len(response['Items']),
                                                                    len(domains),
@@ -57,27 +59,32 @@ def main(event, context):
     obj = bucket.Object(file_name)
     try:
         # unzip the gzip compression and decode to utf-8
-        file_text = (gzip.decompress(obj.get()['Body'].read())).decode('utf-8')
+        file_text = (gzip.decompress(obj.get()['Body'].read())).decode('ascii')
         file_domains = json.loads(file_text)
-        logger.info("Read {} domains from file {}".format(len(file_domains),
-                                                          initials))
+        logger.info("Read {} domains from file {} with size {:,}".format(len(file_domains),
+                                                                         initials,
+                                                                         obj.content_length))
     except ClientError as e:
         # file does not exist in bucket
         if e.response['Error']['Code'] == 'NoSuchKey':
             logger.info("Unable to find existing {} file, proceeding to create")
         else:
-            logger.info("Encountered error with code {}".format(e.response['Error']['Code']))
+            logger.info("Encountered error with code {}, does the file exists?".format(e.response['Error']['Code']))
         file_domains = []
 
-    # unique-ify the entire list/set
+    # unique-ify the entire list/set (DB records + records in S3)
     unique_db_domains.extend(file_domains)
     unique_domains = list(set(unique_db_domains))
     logger.info("Writing {} domains to file {}".format(len(unique_domains),
                                                        initials))
 
-    # write output to file
-    zip_binary = gzip.compress(json.dumps(unique_domains).encode('utf-8'))
+    # write output to file back to s3
+    zip_binary = gzip.compress(json.dumps(unique_domains).encode('ascii'))
     s3_client = boto3.client('s3')
     s3_client.upload_fileobj(io.BytesIO(zip_binary), os.environ['bucket_name'], file_name)
+    head_object = s3_client.head_object(Bucket=os.environ['bucket_name'],
+                                        Key=file_name)
+    logger.info("Uploaded new file: {} , size: {:,}".format(initials,
+                                                            head_object['ContentLength']))
 
     return {"status": 200}
